09a6daf7c9d345e3c697ff461de13acf17ca698e
tgoodwyn <tgoodwyn@gmail.com>
2020-02-04 21:11:20 -0600

    4 datum

--- lab2/lab2.s
+++ lab2/lab2.s
@@ -1,1 +1,12 @@
-putting stuff down
+/******************************************************************************
+* Lab 1
+* CS 413
+* Menon - 01
+* Tyler Goodwyn
+******************************************************************************/
+.data
+
+.balign 4 welcomePrint: .asciz "Welcome. Please select a shape.\n"
+.balign 4 selection: .word 0
+.balign 4 strInputFormat: .asciz "%s"
+.balign 4 areaPrint: .asciz “The area is: %d \n”
